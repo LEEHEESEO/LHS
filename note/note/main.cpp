78d2d5fcaa500addcf6d5ccd2490d50fcb2fb611
LEEHEESEO <107111970+LEEHEESEO@users.noreply.github.com>
2022-06-08 20:57:10 +0900

    Add files via upload

---
+++ note/note/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  note
+//
+//  Created by 이희서 on 2022/06/08.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    printf("This is an assignment of COMPUTER PROGRAMING class.\n");
+    
+    return 0;
+}
